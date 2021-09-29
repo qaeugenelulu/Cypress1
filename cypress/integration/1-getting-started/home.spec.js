a3f2791125323d7d5217e27076d36950c5300f80
qaeugenelulu <etimofeev@lululemon.com>
2021-09-29 11:35:20 -0700

    dffsd

--- cypress/integration/1-getting-started/home.spec.js
+++ cypress/integration/1-getting-started/home.spec.js
@@ -1,17 +1,22 @@
 /// <reference types="cypress" />
-//import HomePage from "/Users/etimofeev/Desktop/automation/Cypress1/cypress/pages/HomePage.js";
+import HomePage from "/Users/etimofeev/Desktop/automation/Cypress1/cypress/pages/HomePage.js";
 
 
-
-   
 
 describe('my first Home page suite',()=> {
 
-   it('open Home page and verify some elements' ,()=>{
+   it('open Home page and verify logo elements' ,()=>{
 
     cy.visit('/')
 
-   })
+    HomePage.homePageLogo().should("be.visible");
+    HomePage.signInLogo().should("be.visible");
+    HomePage.wishLogo().should("be.visible");
+    HomePage.countrylogo().should("be.visible")
+    //HomePage.womenLink().click()
+
+    cy.get('[data-testid=nav-desktop-search]').type('pants{enter}')
+
 
 
 
@@ -24,3 +29,4 @@ describe('my first Home page suite',()=> {
 
 
 
+})
--- cypress/pages/HomePage.js
+++ cypress/pages/HomePage.js
@@ -1,6 +1,25 @@
-import { describe } from "mocha";
+//import { describe } from "mocha";
 
-export default class HomePage 
+export default class HomePage {
 
+ static homePageLogo() {
+    return cy.get(".mega-nav_logo__13GOX");
+  }
 
-  
+  static signInLogo(){
+      return cy.get('.iconComponents_underline__37kK3')
+  }
+
+  static wishLogo(){
+      return cy.get('[class="anchor_anchor__1pPYT iconComponents_iconWrapper__S6T-1 lll-text-body-3"]')
+
+  }
+
+  static countrylogo(){
+         return  cy.get('[data-testid=desktop-region-selector] > .iconComponents_underline__37kK3')
+  }
+  static womenLink(){
+    return  cy.get(':nth-child(1) > [data-testid=nav-desktop-l1]')
+  }
+
+}
